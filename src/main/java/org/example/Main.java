08c829a4f2cae66aae1b7c7fdc17247636bdabce
Evgenia <speaknarcissus@gmail.com>
2025-05-12 14:17:19 +0300

    Поправлена логика класса Main - арифмитические действия

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter> ");
-       String text = new Scanner(System.in).nextLine();
-       System.out.println("Длина текста: "+text.length());
-         }
+        System.out.println("Введите первое число");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int addition = firstNumber + secondNumber;
+        System.out.println("Сумма числа "+firstNumber+" и "+secondNumber+" равна "+addition);
+        int subtraction = firstNumber - secondNumber;
+        System.out.println("Разность числа "+firstNumber+" и "+secondNumber+" равна "+subtraction);
+        int multiplication = firstNumber * secondNumber;
+        System.out.println("Произведение числа "+firstNumber+" и "+secondNumber+" равно "+multiplication);
+        double division = (double)firstNumber / secondNumber;
+        System.out.println("Частное числа "+firstNumber+" и "+secondNumber+" равно "+division);
+    }
 }
