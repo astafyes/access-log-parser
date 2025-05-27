3dec7d13335e2d06f90bab81c2688f36fbc20dd6
Evgenia <speaknarcissus@gmail.com>
2025-05-27 22:09:16 +0300

    В класс Main добавлена логика с проверкой ввода пути к файлу

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,35 @@
 package org.example;
 
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter> ");
-       String text = new Scanner(System.in).nextLine();
-       System.out.println("Длина текста: "+text.length());
-         }
+        Scanner scanner = new Scanner(System.in);
+        int fileNumber = 0;
+        int correctFileCount = 0;
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            String path = scanner.nextLine();
+            File file = new File(path);
+            boolean exists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!exists || isDirectory) {
+                if (!exists) {
+                    System.out.println("Файл не существует по указанному пути.");
+                    System.out.println("Кол-во правильно введенных путей = "+correctFileCount);
+                } else {
+                    System.out.println("Указанный путь является папкой, а не файлом.");
+                    System.out.println("Кол-во правильно введенных путей = "+correctFileCount);
+                }
+                continue;
+            }
+            fileNumber++;
+            correctFileCount++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + fileNumber);
+            System.out.println("Кол-во правильно введенных путей = "+correctFileCount);
+        }
+    }
 }
